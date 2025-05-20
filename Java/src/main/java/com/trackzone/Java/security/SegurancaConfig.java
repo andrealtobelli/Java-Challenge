87d67af59e7f530dd54f0d264b8c36723760528d
André Antunes <161328841+andrealtobelli@users.noreply.github.com>
2025-05-20 18:18:31 -0300

    Update SegurancaConfig.java

--- Java/src/main/java/com/trackzone/Java/security/SegurancaConfig.java
+++ Java/src/main/java/com/trackzone/Java/security/SegurancaConfig.java
@@ -11,25 +11,33 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SegurancaConfig {
 
     @Autowired
     private JWTAuthFilter jwtAuthFilter;
-    
+
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class).build();
     }
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions(frame -> frame.disable())) // Libera o uso de iframe (necessário pro H2 Console)
             .authorizeHttpRequests(auth -> auth
-            		.requestMatchers("/filiais/login", "/filiais/cadastrar", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .anyRequest().authenticated())
+                .requestMatchers(
+                    new AntPathRequestMatcher("/filiais/login"),
+                    new AntPathRequestMatcher("/filiais/cadastrar"),
+                    new AntPathRequestMatcher("/swagger-ui/**"),
+                    new AntPathRequestMatcher("/v3/api-docs/**"),
+                    new AntPathRequestMatcher("/h2-console/**")
+                ).permitAll()
+                .anyRequest().authenticated()
+            )
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
@@ -40,4 +48,4 @@ public class SegurancaConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-} 
+}

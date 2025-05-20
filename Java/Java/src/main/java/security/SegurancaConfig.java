70f9b3e8be0c9bad14580dcf7b01a23403d6a2f1
André Antunes <161328841+andrealtobelli@users.noreply.github.com>
2025-05-20 17:05:13 -0300

    Update SegurancaConfig.java

--- Java/Java/src/main/java/security/SegurancaConfig.java
+++ Java/Java/src/main/java/security/SegurancaConfig.java
@@ -1,44 +1,50 @@
-package security;
+package com.trackzone.Java.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SegurancaConfig {
 
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
+
     @Bean
-    SecurityFilterChain filtrarRota(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .headers(headers -> headers.frameOptions(frame -> frame.disable())) // Permite iframes do H2 Console
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
-                    "/auth/**",
-                    "/h2-console/**",
-                    "/swagger-ui/**",
-                    "/swagger-ui.html",
-                    "/v3/api-docs/**"
+                    "/filiais/login",
+                    "/filiais/cadastrar",
+                    "/swagger-ui/",
+                    "/v3/api-docs/",
+                    "/h2-console/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
-            .httpBasic(Customizer.withDefaults());
+            .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
     @Bean
-    UserDetailsService gerarUsuario() {
-        return new InMemoryUserDetailsManager(
-            User.withUsername("admin")
-                .password("{noop}1234")
-                .roles("ADMIN")
-                .build()
-        );
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
